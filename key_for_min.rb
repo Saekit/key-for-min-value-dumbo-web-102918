e9f711243fb46b5e98b29152a5288a956c814bab
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-30 21:31:16 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,18 +4,11 @@
 
 def key_for_min_value(name_hash)
   smallest_value = nil
-  return nil if name_hash.empty?
-  name_hash.each do |key, value|
-    return key if value < smallest_value
-  end
-end
-def key_for_min_value(hash)
   smallest_key = nil
-  tiniest_value = nil
-  hash.each do |key, value|
-    if tiniest_value == nil || value < tiniest_value
-      tiniest_value = value
-      smallest_key = key
+  name_hash.each do |key, value|
+    if smallest_value == nil || value < smallest_value
+      smallest_value = value
+      smallest_key = key 
     end
   end
   smallest_key
