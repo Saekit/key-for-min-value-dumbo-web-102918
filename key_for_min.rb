3d2f5fa8cb3b70006cbe4bcb749eb1f6e3851080
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-30 21:29:40 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 # If the method is called and passed an argument of an empty hash, it should return nil.
 
-# def key_for_min_value(name_hash)
-#   smallest_value = nil
-#   return nil if name_hash.empty?
-#   name_hash.each do |key, value|
-#     return key if value < smallest_value
-#   end
-# end
+def key_for_min_value(name_hash)
+  smallest_value = nil
+  return nil if name_hash.empty?
+  name_hash.each do |key, value|
+    return key if value < smallest_value
+  end
+end
 def key_for_min_value(hash)
   smallest_key = nil
   tiniest_value = nil
