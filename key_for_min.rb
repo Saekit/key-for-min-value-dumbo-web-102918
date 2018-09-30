cc113d2b92b375fbb7ab2a99ad9e140d3b1aa38e
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-30 21:11:55 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+# If the method is called and passed an argument of an empty hash, it should return nil.
 
 def key_for_min_value(name_hash)
-
+  
 end
